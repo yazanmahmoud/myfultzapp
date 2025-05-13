811e2aca13efe34fe1a8a6cf1b31bcd9ddcd4ca4
Yazan Mahmoud <yazan.shaker@gmail.com>
2025-05-13 20:27:52 -0300

    initial commit

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ import './App.css';
 
 function App() {
   return (
-    <Router>
+    <Router basename='/myfultzapp'>
       <Layout>
         <Routes>
           <Route path="/" element={<Home />} />
