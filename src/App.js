3b79d92eb9c39a19f10f18fa112bfd335bce9daf
Yazan Mahmoud <yazan.shaker@gmail.com>
2025-05-15 16:39:55 -0300

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { HashrRouter as Router, Routes, Route } from 'react-router-dom';
 import Footer from './components/Footer';
 import Home from './pages/Home';
 import About from './pages/About';
