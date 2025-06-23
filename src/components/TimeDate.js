bfb2d7f8ee6e8db0a3fdd2318ff374c382585d9f
Yazan Mahmoud <yazan.shaker@gmail.com>
2025-06-23 18:58:02 -0300

    initial commit

--- src/components/TimeDate.js
+++ src/components/TimeDate.js
@@ -9,7 +9,7 @@ const TimeDate = () => {
   }, []);
 
   return (
-    <div style={{ fontSize: '1.5em', marginBottom: '15px', marginRight: '25px', marginTop: '12px' }}>
+    <div style={{ fontSize: '1.2em', marginBottom: '15px', marginRight: '25px', marginTop: '20px' }}>
       {dateTime.toLocaleDateString()}
        <br></br>
       {dateTime.toLocaleTimeString()}
